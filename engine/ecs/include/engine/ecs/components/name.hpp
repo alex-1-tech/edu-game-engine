eb279ad230115c1053a27b362f2f49a6940ff8ff
alex <chistovreal@gmail.com>
2026-01-23 20:01:36 +0300

    [ECS] Add basic components (Name, Transform, Velocity)
    - Add Name component for entity identification
    - Add Transform component for position and rotation
    - Add Velocity component for movement data
    - These components form the basis for entity behavior

---
+++ engine/ecs/include/engine/ecs/components/name.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "engine/ecs/component.hpp"
+
+EGE_NAMESPACE_BEGIN
+
+class Name : public Component<Name>
+{
+public:
+  Name() = default;
+  Name(String name)
+      : m_name(std::move(name))
+  {
+  }
+
+  [[nodiscard]] auto name() const -> const String& { return m_name; }
+  void setName(const String& name) { m_name = name; }
+
+  [[nodiscard]] auto toString() const -> String override { return fmt::format("Name(\"{}\")", m_name); }
+
+  [[nodiscard]] auto explain() const -> String override
+  {
+    return fmt::format("Name Component:\n"
+                       "└─ Name: \"{}\" - human-readable identifier for entity",
+                       m_name);
+  }
+
+private:
+  String m_name;
+};
+
+EGE_NAMESPACE_END
